86f6cc36a58fea9f8f105e6221133649eedcb797
srena1993 <kosarajusrena99@gmail.com>
2026-03-26 11:22:40 -0400

    Create Payment.java

---
+++ payment-service/src/main/java/com/example/paymentservice/entity/Payment.java
@@ -0,0 +1,20 @@
+package com.example.paymentservice.entity;
+
+public class Payment {
+
+    private Long id;
+    private String status;
+
+    public Payment(Long id, String status) {
+        this.id = id;
+        this.status = status;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+}
